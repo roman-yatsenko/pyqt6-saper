d60f148e601bfd7f65292b5a8fb59c9d2bf71234
YaRo <roman.yatsenko@hneu.net>
2023-05-27 14:37:16 +0300

    Add flag toggling

--- saper.py
+++ saper.py
@@ -14,6 +14,7 @@ LEVELS = (
 IMG_BOMB = QImage('./images/bomb.png')
 IMG_CLOCK = QImage('./images/clock.png')
 IMG_START = QImage('./images/rocket.png')
+IMG_FLAG = QImage('./images/flag.png')
 
 STATUS_READY = 0
 STATUS_PLAY = 1
@@ -69,6 +70,8 @@ class Cell(QWidget):
                 f.setBold(True)
                 p.setFont(f)
                 p.drawText(r, Qt.AlignmentFlag.AlignCenter, str(self.mines_around))
+        elif self.is_flagged:
+            p.drawPixmap(r, QPixmap(IMG_FLAG))
 
     def reset(self):
         self.is_start = False
@@ -100,6 +103,13 @@ class Cell(QWidget):
         self.clicked.emit()
         if event.button() == Qt.MouseButton.LeftButton:
             self.click()
+        elif event.button() == Qt.MouseButton.RightButton:
+            if not self.is_revealed:
+                self.toggle_flag()
+    
+    def toggle_flag(self):
+        self.is_flagged = not self.is_flagged
+        self.update()
 
 
 class MainWindow(QMainWindow):

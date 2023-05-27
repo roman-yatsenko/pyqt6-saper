6b799efd3e2a443992489682cd52617137ec21f6
YaRo <roman.yatsenko@hneu.net>
2023-05-27 14:24:19 +0300

    Add game over

--- saper.py
+++ saper.py
@@ -30,6 +30,7 @@ STATUS_ICONS = {
 class Cell(QWidget):
     expandable = pyqtSignal(int, int)
     clicked = pyqtSignal()
+    game_over = pyqtSignal()
 
     def __init__(self, x, y):
         super().__init__()
@@ -46,6 +47,8 @@ class Cell(QWidget):
         if self.is_revealed:
             color = self.palette().color(QPalette.ColorRole.NColorRoles.Window)
             outer, inner = color, color
+            if self.is_end:
+                inner = Qt.GlobalColor.black
         else:
             outer, inner = Qt.GlobalColor.gray, Qt.GlobalColor.lightGray
         p.fillRect(r, QBrush(inner))
@@ -85,6 +88,9 @@ class Cell(QWidget):
             self.reveal_self()
             if self.mines_around == 0:
                 self.expandable.emit(self.x, self.y)
+            if self.is_mine:
+                self.is_end = True
+                self.game_over.emit()
 
     def reveal_self(self):
         self.is_revealed = True
@@ -168,6 +174,7 @@ class MainWindow(QMainWindow):
                 self.grid.addWidget(cell, x, y)
                 cell.expandable.connect(self.expand_reveal)
                 cell.clicked.connect(self.handle_click)
+                cell.game_over.connect(self.game_over)
 
     def reset(self):
         self.mines_count = LEVELS[self.level][1]
@@ -246,6 +253,10 @@ class MainWindow(QMainWindow):
         if self.status == STATUS_PLAY:
             n_seconds = int(time.time()) - self._timer_start
             self.clock.setText(f'{n_seconds:03d}')
+
+    def game_over(self):
+        self.update_status(STATUS_FAILED)
+
 
 if __name__ == '__main__':
     app = QApplication([])
